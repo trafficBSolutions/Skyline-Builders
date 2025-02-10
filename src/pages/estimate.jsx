ecea81d23d0223c0e35467a3f8d534a60be09c12
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-02-10 13:27:57 -0500

    Update estimate.jsx

--- src/pages/estimate.jsx
+++ src/pages/estimate.jsx
@@ -380,7 +380,7 @@ const Estimate = () => {
                     <h2 className="footer-office-h2">Home Office</h2>
                     <div className="address-office">
                         <p className="address-p">
-                        306 HILLSIDE DR SW
+                        763 HWY 53
                         CALHOUN, GA 30701  
                         </p>
                     </div>
